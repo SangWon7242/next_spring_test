e5ceb96ae59e10cbee8c9184da62af7f7ee3c45a
SangWon7242 <gotkdqja@gmail.com>
2023-08-16 20:27:26 +0900

    테일윈드 css 적용

--- src/main/frontend/src/App.js
+++ src/main/frontend/src/App.js
@@ -1,5 +1,7 @@
+import "./App.css";
 import React, {useEffect, useState} from 'react';
 import axios from 'axios';
+import { Link } from 'react-router-dom'
 
 function App() {
    const [hello, setHello] = useState('')
@@ -11,9 +13,25 @@ function App() {
     }, []);
 
     return (
-        <div>
-            <span>백엔드에서 가져온 데이터입니다 : {hello}</span>
-        </div>
+        <>
+            <header className="h-[60px] bg-[#dfdfdf]">
+               <div className="con mx-auto flex h-full w-[1200px]">
+                 <nav className="menu-1 flex self-center">
+                   <ul className="flex h-full">
+                     <li>
+                       <a href="/" className="block px-4 hover:text-[#afafaf]">메인</a>
+                     </li>
+                     <li>
+                        <a href="#" className="block px-4 hover:text-[#afafaf]">어바웃</a>
+                     </li>
+                     <li>
+                       <a href="#" className="block px-4 hover:text-[#afafaf]">연락처</a>
+                     </li>
+                   </ul>
+                 </nav>
+               </div>
+             </header>
+        </>
     );
 }
 
