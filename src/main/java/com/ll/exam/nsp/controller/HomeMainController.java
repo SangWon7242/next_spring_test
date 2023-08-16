bb621807a2575960d02f65a5c2a2695b466f596a
SangWon7242 <gotkdqja@gmail.com>
2023-08-16 19:23:27 +0900

    open jdk 버전 11로 변경, 빌드그레들에 자동 빌드 코드 추가

--- src/main/java/com/ll/exam/nsp/controller/HomeMainController.java
+++ src/main/java/com/ll/exam/nsp/controller/HomeMainController.java
@@ -1,14 +1,12 @@
 package com.ll.exam.nsp.controller;
 
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
 
-@Controller
+@RestController // @RestController = @Controller + @ResponseBody
 public class HomeMainController {
 
   @GetMapping("/hello")
-  @ResponseBody
   public String hello() {
     return "Hello, world!";
   }
